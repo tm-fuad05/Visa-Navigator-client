63401518005fc21395cae0005e0ea560c78fd53c
tm-fuad05 <tm.fuad05@gmail.com>
2024-12-06 03:14:05 +0600

    height customized

--- src/Components/Home/ExtraSection2.jsx
+++ src/Components/Home/ExtraSection2.jsx
@@ -16,17 +16,17 @@ const ExtraSection2 = () => {
 
   return (
     <div className="w-11/12 mx-auto mb-10 mt-72 md:mt-0">
-      <div className="flex flex-col items-center  w-7/12 mx-auto text-center">
+      <div className="flex flex-col items-center w-10/12  lg:w-7/12 mx-auto text-center mb-5">
         <p className="text-[8px]">CLIENTS TESTIMONIALS</p>
         <h2 className="text-2xl lg:text-4xl font-bold">
           What Customers Saying About Visarzo
         </h2>
         <RxBorderSolid className="text-5xl text-blue-600" />
       </div>
-      <div className="w-9/12 mx-auto  grid grid-cols-1 md:grid-cols-2 gap-5">
+      <div className="w-11/12 mx-auto  grid grid-cols-1 md:grid-cols-2 gap-5">
         {reviews.map((review, idx) => (
           <div key={idx}>
-            <div className="border p-5 h-64 md:h-72 lg:h-48 flex  flex-col lg:flex-row  items-center gap-5 hover:scale-105 duration-500">
+            <div className="border p-5 h-72 md:h-72 lg:h-48 flex  flex-col lg:flex-row  items-center gap-5 hover:scale-105 duration-500">
               <div className="flex flex-col  items-center lg:w-1/3">
                 <figure className="w-12 h-12 ">
                   <img
